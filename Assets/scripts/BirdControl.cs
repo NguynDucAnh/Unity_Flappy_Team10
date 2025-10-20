b1a87e4d62b5c6ab546a7827eb461c74d63f28bc
Vu Van Duc <211147022+vuvanduc1234@users.noreply.github.com>
2025-10-20 19:28:05 +0700

    fix bug error get poin, fix ui

--- Assets/scripts/BirdControl.cs
+++ Assets/scripts/BirdControl.cs
@@ -2,11 +2,13 @@
 using System.Collections;
 using DG.Tweening;
 
-public class BirdControl : MonoBehaviour {
+public class BirdControl : MonoBehaviour
+{
 
-	public int rotateRate = 10;
-	public float upSpeed = 10;
+    public int rotateRate = 10;
+    public float upSpeed = 10;
     public GameObject scoreMgr;
+    public GameObject gameOverPanel;
 
     public AudioClip jumpUp;
     public AudioClip hit;
@@ -14,13 +16,14 @@ public class BirdControl : MonoBehaviour {
 
     public bool inGame = false;
 
-	private bool dead = false;
-	private bool landed = false;
+    private bool dead = false;
+    private bool landed = false;
 
     private Sequence birdSequence;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         float birdOffset = 0.05f;
         float birdTime = 0.3f;
         float birdStartY = transform.position.y;
@@ -32,41 +35,42 @@ public class BirdControl : MonoBehaviour {
             .Append(transform.DOMoveY(birdStartY, birdTime).SetEase(Ease.Linear))
             .SetLoops(-1);
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         if (!inGame)
         {
             return;
         }
         birdSequence.Kill();
 
-		if (!dead)
-		{
-			if (Input.GetButtonDown("Fire1"))
-			{
+        if (!dead)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
                 JumpUp();
-			}
-		}
+            }
+        }
 
-		if (!landed)
-		{
-			float v = transform.GetComponent<Rigidbody2D>().velocity.y;
-			
-			float rotate = Mathf.Min(Mathf.Max(-90, v * rotateRate + 60), 30);
-			
-			transform.rotation = Quaternion.Euler(0f, 0f, rotate);
-		}
-		else
-		{
-			transform.GetComponent<Rigidbody2D>().rotation = -90;
-		}
-	}
+        if (!landed)
+        {
+            float v = transform.GetComponent<Rigidbody2D>().velocity.y;
 
-	void OnTriggerEnter2D (Collider2D other)
-	{
-		if (other.name == "land" || other.name == "pipe_up" || other.name == "pipe_down")
-		{
+            float rotate = Mathf.Min(Mathf.Max(-90, v * rotateRate + 60), 30);
+
+            transform.rotation = Quaternion.Euler(0f, 0f, rotate);
+        }
+        else
+        {
+            transform.GetComponent<Rigidbody2D>().rotation = -90;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "land" || other.name == "pipe_up" || other.name == "pipe_down")
+        {
             if (!dead)
             {
                 GameObject[] objs = GameObject.FindGameObjectsWithTag("movable");
@@ -77,36 +81,46 @@ public class BirdControl : MonoBehaviour {
 
                 GetComponent<Animator>().SetTrigger("die");
                 AudioSource.PlayClipAtPoint(hit, Vector3.zero);
+
+                // Hiển thị Game Over Panel sau một khoảng delay nhỏ
+                StartCoroutine(ShowGameOverDelay());
             }
 
-			
+            if (other.name == "land")
+            {
+                transform.GetComponent<Rigidbody2D>().gravityScale = 0;
+                transform.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
 
-			if (other.name == "land")
-			{
-				transform.GetComponent<Rigidbody2D>().gravityScale = 0;
-				transform.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
-
-				landed = true;
-			}
-		}
+                landed = true;
+            }
+        }
 
         if (other.name == "pass_trigger")
         {
             scoreMgr.GetComponent<ScoreMgr>().AddScore();
             AudioSource.PlayClipAtPoint(score, Vector3.zero);
         }
+    }
 
+    IEnumerator ShowGameOverDelay()
+    {
+        yield return new WaitForSeconds(1f);
 
-	}
+        if (gameOverPanel != null)
+        {
+            int currentScore = scoreMgr.GetComponent<ScoreMgr>().GetCurrentScore();
+            gameOverPanel.GetComponent<GameOverPanel>().ShowGameOver(currentScore);
+        }
+    }
 
     public void JumpUp()
     {
         transform.GetComponent<Rigidbody2D>().velocity = new Vector2(0, upSpeed);
         AudioSource.PlayClipAtPoint(jumpUp, Vector3.zero);
     }
-	
-	public void GameOver()
-	{
-		dead = true;
-	}
+
+    public void GameOver()
+    {
+        dead = true;
+    }
 }
